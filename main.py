c812d938d4dc42500648438e7326cb33d9feba77
chou <choukatanagari@yahoo.com>
2022-09-23 13:46:09 +0800

    first

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from flask import (Flask, render_template, jsonify, request, url_for)
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+@app.route("/api/node")
+def api_node():
+    return jsonify()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
